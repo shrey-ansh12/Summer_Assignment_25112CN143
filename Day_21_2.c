96d81aa39100ce0de739487c83321e08e66ba73e
Shreyansh Baranwal <baranwalshreyansh75@gmail.com>
2026-06-21 14:55:16 +0530

    Daay_21

---
+++ Day_21_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], temp;
+    int i, length;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    length = strlen(str);
+
+    if (str[length - 1] == '\n') {
+        str[length - 1] = '\0';
+        length--;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
